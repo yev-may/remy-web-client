7a5a3f61de6e2b4d24225d5693123c63ac8e0536
Yevay <yevhenii.may@gmail.com>
2022-12-22 15:12:12 +0200

    Add page routing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <script>
 import Header from './components/header/Header.vue'
-import Main from './components/main/Main.vue'
+import Welcome from './pages/Welcome.vue'
+import About from './pages/About.vue'
+
+const routes = {
+  '/': Welcome,
+  '/about': About
+}
 
 export default {
-  components : {
-    Header,
-    Main
+  components: {
+    Header
+  },
+  data() {
+    return {
+      currentPath: window.location.pathname
+    }
+  },
+  computed: {
+    header() {
+      return Header
+    },
+    currentPage() {
+      return routes[this.currentPath]
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.pathname
+    })
   }
 }
 </script>
 
 <template>
   <Header />
-  <Main />
+  <component :is="currentPage"/>
 </template>
 
 <style>
