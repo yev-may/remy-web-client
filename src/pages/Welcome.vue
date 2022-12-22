ecb8b65e57a0751c78e0188162b089aeef98f2ee
Yevay <yevhenii.may@gmail.com>
2022-12-22 16:55:52 +0200

    Add welcome page content

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,3 +1,9 @@
 <template>
-    Welcome
+    <div class="container full-height">
+        <div class="row align-items-center full-height">
+            <div class="col text-center">
+                <p class="fs-1">Welcome!</p>
+            </div>
+        </div>
+    </div>
 </template>
