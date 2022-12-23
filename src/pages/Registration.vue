84e1efebeb00b110b9683ccd92bd21cf56b3f98d
Yevay <yevhenii.may@gmail.com>
2022-12-23 11:38:22 +0200

    Add registration request

--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -5,18 +5,47 @@
                 <div class="mt-5 mb-3 text-center">
                     <h3 >Registration</h3>
                 </div>
-                <form>
+                <form id="registrationForm">
                     <div class="my-2">
                         <label for="login" class="form-label">Login</label>
-                        <input type="text" class="form-control" id="login">
+                        <input v-model="userForm.login" type="text" class="form-control" id="login">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password">
+                        <input v-model="userForm.password" type="password" class="form-control" id="password">
                     </div>
-                    <button type="submit" class="btn w-100 btn-theme">Create account</button>
                 </form>
+                <button @click="sendRegistrationRequest()" class="btn w-100 btn-theme">Create account</button>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+
+const REGISTRATION_URL = 'http://localhost:8080/user/register'
+
+export default {
+    data() {
+        return {
+            userForm : {
+                login: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        sendRegistrationRequest() {
+            fetch(REGISTRATION_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(this.userForm)
+            })
+            .then(response => response.json())
+            .then(responseJson => console.log(responseJson));
+        }
+    }
+}
+</script>
