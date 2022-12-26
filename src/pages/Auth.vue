44eaec01f4a67b165ce013357403ce73e1a1e39c
Yevay <yevhenii.may@gmail.com>
2022-12-26 19:38:19 +0200

    Implement login logic

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -5,16 +5,16 @@
                 <div class="mt-5 mb-3 text-center">
                     <h3 >Authorization</h3>
                 </div>
-                <form>
+                <form id="loginForm" @submit.prevent="submit">
                     <div class="my-2">
-                        <label for="login" class="form-label">Login</label>
-                        <input type="text" class="form-control" id="login">
+                        <label for="login" class="form-label">Email</label>
+                        <input v-model="userForm.email" type="text" class="form-control" id="login">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password">
+                        <input v-model="userForm.password" type="password" class="form-control" id="password">
                     </div>
-                    <button type="submit" class="btn w-100 btn-theme">Sign in</button>
+                    <button @click="sendLoginRequest()" class="btn w-100 btn-theme">Sign in</button>
                 </form>
                 <div class="mt-3 text-center">
                     <a href="/registration">Create account</a>
@@ -23,3 +23,33 @@
         </div>
     </div>
 </template>
+
+<script>
+
+const LOGIN_URL = 'http://localhost:8080/user/login'
+
+export default {
+    data() {
+        return {
+            userForm : {
+                email: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        sendLoginRequest() {
+            fetch(LOGIN_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'cookie': document.cookie
+                },
+                body: JSON.stringify(this.userForm)
+            })
+            .then(response => response.json())
+            .then(responseJson => console.log(responseJson));
+        }
+    }
+}
+</script>
