09372dbc46684df4fa6d195f3e4eefb43c8e8b1c
Yevay <yevhenii.may@gmail.com>
2023-02-10 00:01:10 +0200

    Add registration validation

--- src/pages/Registration.vue
+++ src/pages/Registration.vue
@@ -3,17 +3,29 @@
         <h3 >Registration</h3>
     </div>
     <form @submit.prevent="submit" id="registrationForm">
+        <div class="alert alert-warning mt-1" v-for="error in objectErrors">
+                <span>{{ error }}</span>
+            </div>
         <div class="my-2">
             <label for="login" class="form-label">Email</label>
             <input v-model="userForm.email" type="text" class="form-control" id="login">
+            <div class="alert alert-warning mt-1" v-if="emailErrorMessage != null">
+                <span>{{ emailErrorMessage }}</span>
+            </div>
         </div>
         <div class="my-2">
             <label for="login" class="form-label">Login</label>
             <input v-model="userForm.login" type="text" class="form-control" id="login">
+            <div class="alert alert-warning mt-1" v-if="loginErrorMessage != null">
+                <span>{{ loginErrorMessage }}</span>
+            </div>
         </div>
         <div class="mb-2">
             <label for="password" class="form-label">Password</label>
             <input v-model="userForm.password" type="password" class="form-control" id="password">
+            <div class="alert alert-warning mt-1" v-if="passwordErrorMessage != null">
+                <span>{{ passwordErrorMessage }}</span>
+            </div>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Repeat password</label>
@@ -24,6 +36,7 @@
 </template>
 
 <script>
+import { requestFactory } from './../module/requestFactory.js'
 
 const REGISTRATION_URL = 'http://localhost:8080/user/register'
 
@@ -35,20 +48,47 @@ export default {
                 login: "",
                 password: "",
                 repeatPassword: ""
-            }
+            },
+            fieldErrors: [],
+            objectErrors: []
         }
     },
-    methods: {
+
+    computed: 
+    {
+        emailErrorMessage() {
+            return this.getErrorMessage('email');
+        },
+
+        loginErrorMessage() {
+            return this.getErrorMessage('login');
+        },
+
+        passwordErrorMessage() {
+            return this.getErrorMessage('password');
+        },
+    },
+    
+    methods: 
+    {
         sendRegistrationRequest() {
-            fetch(REGISTRATION_URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                body: JSON.stringify(this.userForm)
-            })
-            .then(response => response.json())
-            .then(responseJson => console.log(responseJson));
+            requestFactory.postJsonRequest(REGISTRATION_URL, this.userForm)
+                .then(response => response.json())
+                .then(responseJson => this.validateResponse(responseJson))
+                .then(responseJson => console.log(responseJson));
+        },
+
+        validateResponse(responseJson) {
+            this.fieldErrors = responseJson.fieldErrors;
+            this.objectErrors = responseJson.objectErrors;
+        },
+
+        getErrorMessage(field) {
+            for (let error of this.fieldErrors) {
+                if(error.field == field) {
+                    return error.message;
+                }
+            }
         }
     }
 }
