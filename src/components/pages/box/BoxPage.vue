3f515b0e3e0c69b629b7cf0c8a5e24d2cf571374
Yevay <yevhenii.may@gmail.com>
2023-03-05 15:51:24 +0200

    Add sample box page

--- src/components/pages/box/BoxPage.vue
+++ src/components/pages/box/BoxPage.vue
@@ -1,6 +1,22 @@
 <template>
     <TopContainer>
-        <h2 class="text-center">{{ box.title }}</h2>
+        <div class="mb-3">
+            <h3 class="text-center">{{ box.title }}</h3>
+            <div>
+                <a class="btn w-100 btn-theme" href="">Details</a>
+            </div> 
+        </div>
+        
+        <div class="mb-3">
+            <p class="mb-2">New cards today: {{ box.cardsAddedToday }}</p>
+            <a class="btn w-100 btn-theme" href="">New card</a>
+        </div>
+        
+        <div class="mb-4">
+            <p class="mb-2">Cards to repeat for today: {{ box.cardsToRepeatToday }}</p>
+            <a class="btn w-100 btn-theme" href="">Start repetition</a>
+        </div>
+        
     </TopContainer>
 </template>
 
@@ -11,11 +27,12 @@ export default {
     data() {
         return {
             box: {
-                title: 'Test 1'
+                title: 'Test 1',
+                cardsAddedToday: 5,
+                cardsToRepeatToday: 5
             }
         }
     },
-
     components: { TopContainer }
 }
 </script>
